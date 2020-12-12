2a3ca97c3d657d5bcd1955e9ab41cbe18e9a28f8
Laimonas Masionis <71811684+LaimonasMas@users.noreply.github.com>
2020-12-12 22:24:41 +0200

    input validation

--- js/components/myServices/areServicesValid.js
+++ js/components/myServices/areServicesValid.js
@@ -1,26 +1,26 @@
 function areServicesValid(services) {
     if (typeof services !== 'object') {
-        console.error('ERROR: ...');
+        console.error('ERROR: services should be an object type');
         return false;
     }
     if (typeof services.icon !== 'string') {
-        console.error('ERROR: ...');
+        console.error('ERROR: icon data should be a string type');
         return false;
     }
-    if (typeof services.icon.length < 1) {
-        console.error('ERROR: ...');
+    if (typeof services.icon.length === 0 ) {
+        console.error('ERROR: icon data length should be equal or longer then 1');
         return false;
     }
     if (typeof services.icon.length > 50) {
-        console.error('ERROR: ...');
+        console.error('ERROR: icon data length cannot be longer then 50');
         return false;
     }
     if (typeof services.title !== 'string') {
-        console.error('ERROR: ...');
+        console.error('ERROR: title should be a string type');
         return false;
     }
     if (typeof services.text !== 'string') {
-        console.error('ERROR: ...');
+        console.error('ERROR: text should be a string type');
         return false;
     }
 
