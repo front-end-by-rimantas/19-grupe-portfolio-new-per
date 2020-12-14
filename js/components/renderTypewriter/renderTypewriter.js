fdab0653bc171a741e91c34af44b470aacd1fdef
Gvidas <gvisis@gmail.com>
2020-12-14 13:12:02 +0200

    prints all the words with bugs

--- js/components/renderTypewriter/renderTypewriter.js
+++ js/components/renderTypewriter/renderTypewriter.js
@@ -1,17 +1,34 @@
+const sarasas = ['photographer','web developer','ui/ux designer'];
 let wordCount = 0;
-const speed = 200;
-const sarasas = ['algis','nijole'];
 let currentWord = '';
 let index = 0;
+const speed = 500;
 const DOM = document.querySelector('li');
 
+// console.log(sarasas);
+// console.log(sarasas[0]);
+// console.log(sarasas[0].length);
+// console.log(sarasas[1]);
+// console.log(sarasas[1].length);
+// console.log(sarasas[2]);
+// console.log(sarasas[2].length);
+
 function renderTypewriter(){
-    if (wordCount < sarasas[index].length){
-        currentWord = sarasas[0].charAt([wordCount]);
-        DOM.textContent += currentWord;
-        ++wordCount;
+    if (wordCount === sarasas[index].length){
+        index++;
         setTimeout(renderTypewriter, speed);
-    }    
+        wordCount = 0;
+        DOM.textContent = '';
+    }
+    if (wordCount < sarasas[index].length){
+        currentWord = sarasas[index].charAt([wordCount]);
+        DOM.textContent += currentWord;
+        wordCount++;
+        setTimeout(renderTypewriter, speed);
+    }
+    console.log(wordCount);
+    console.log(sarasas[index]);
 }
 
 export { renderTypewriter };
+
