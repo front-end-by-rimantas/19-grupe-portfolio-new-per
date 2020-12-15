6327d982eeaa4a12675b6fb592b8763002932d56
VaidaEG <grausliene.vaida@gmail.com>
2020-12-15 12:56:29 +0200

    Client counter validation

---
+++ js/components/clientCounter/isValidInput.js
@@ -0,0 +1,25 @@
+/**
+ * renderClientCounter() funkcijai skirtu ivesties duomenu (input params) validacija.
+ * @param {string} selector Selektorius, kaip rasti norima vieta, kur bus istatomas sugeneruotas kodas
+ * @param {Array} data  Duomenu masyvas su objektais, kurie reprezentuoja klientu skaiciuokle
+ * @returns {boolean} Jei patikrinimo metu randama logine duomenu klaida, grazinama `false`, priesingu atveju - `true`
+ */
+function isInputValid(selector, data) {
+    if (typeof selector !== 'string') {
+        console.error('ERROR: selektorius turi būti tekstinio tipo.')
+    }
+    if (typeof selector === '') {
+        console.error('ERROR: selektorius negali būti tuščias.')
+    }
+    if (!Array.isArray(data)) {
+        console.error('ERROR: Klientų skaičiuoklei generuoti reikia array tipo duomenų.');
+        return false;
+    }
+    if (data.length === 0) {
+        console.error('ERROR: klientų skaičiuoklei generuoti reikia ne tuščio array tipo duomenų sąrašo.');
+        return false;
+    }
+    return true;
+}
+
+export { isInputValid }
---
+++ js/components/clientCounter/renderClientCounter.js
@@ -0,0 +1,13 @@
+import { isValidInput } from './isValidInput.js';
+import { isValidClientCounterItem } from './isValidClientCounterItem.js';
+/**
+ * Klientu skaiciuokles generavimas is pateiktu duomenu i nurodyta vieta DOM'e.
+ * @param {string} selector Selektorius, kaip rasti norima vieta, kur bus istatomas sugeneruotas kodas
+ * @param {Array} data Duomenu masyvas su objektais, kurie reprezentuoja social nuorodas
+ * @returns {boolean} Logikos vykdymo metu radus klaida grazinamas `false`, o funkcijai suveikus teisingai - `true`
+ */
+function renderClientCounter (selector, data) {
+return true;
+}
+
+export { renderClientCounter }
