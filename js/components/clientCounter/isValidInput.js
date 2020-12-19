1cbc2f9036fbe33a17feaad3d093d86d0b3795fe
VaidaEG <grausliene.vaida@gmail.com>
2020-12-19 16:11:58 +0200

    Image validation and selector, data and object validation update in client counter #148

--- js/components/clientCounter/isValidInput.js
+++ js/components/clientCounter/isValidInput.js
@@ -1,3 +1,4 @@
+import { Validator } from '../validator/Validator.js';
 /**
  * renderClientCounter() funkcijai skirtu ivesties duomenu (input params) validacija.
  * @param {string} selector Selektorius, kaip rasti norima vieta, kur bus istatomas sugeneruotas kodas
@@ -5,18 +6,12 @@
  * @returns {boolean} Jei patikrinimo metu randama logine duomenu klaida, grazinama `false`, priesingu atveju - `true`
  */
 function isValidInput(selector, data) {
-    if (typeof selector !== 'string') {
-        console.error('ERROR: selektorius turi būti tekstinio tipo.')
-    }
-    if (typeof selector === '') {
-        console.error('ERROR: selektorius negali būti tuščias.')
-    }
-    if (!Array.isArray(data)) {
-        console.error('ERROR: Klientų skaičiuoklei generuoti reikia array tipo duomenų.');
+    if (!Validator.isSelector(selector)) {
+        console.error('ERROR: selektorius turi būti tekstinio tipo ir negali būti tuščias.')
         return false;
     }
-    if (data.length === 0) {
-        console.error('ERROR: klientų skaičiuoklei generuoti reikia ne tuščio array tipo duomenų sąrašo.');
+    if (!Validator.isArray(data)) {
+        console.error('ERROR: Klientų skaičiuoklei generuoti reikia ne tuščio array tipo duomenų.')
         return false;
     }
     return true;
--- js/components/validator/Validator.js
+++ js/components/validator/Validator.js
@@ -23,6 +23,31 @@ class Validator {
         }
         return true;
     }
+
+    static isImageFile(text) {
+        const validationExtension = ['png', 'jpg', 'gif', 'jpeg'];
+
+        // pavadinimas.pletinys
+        
+        if (typeof text !== 'string' ||
+            text.length < 5 ||
+            text.length > 100 ||
+            text[0] === '.' ||
+            text[text.length - 1] === '.') {
+            return false;
+        }
+        const parts = text.split('.');
+
+        // isitikanme ar tinkama galune
+        if (parts.length !== 2) {
+            return false;
+        }
+        if (!validationExtension.includes(parts[1])) {
+            return false;
+        }
+        // isitikiname ar tinkamas pavadinimas
+        return true;
+    }
 }
 
 export { Validator }
