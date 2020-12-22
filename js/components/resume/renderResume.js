212735bf4c705e74c5c072222a8c772566275f94
rudzyl <73877213+rudzyl@users.noreply.github.com>
2020-12-22 11:38:08 +0200

    resume icons insert adjacent

--- js/components/resume/renderResume.js
+++ js/components/resume/renderResume.js
@@ -5,13 +5,6 @@
  * @returns {*}
  */
 function renderResume(selector, resumeData) {
-    
-     const DOM = document.querySelector(selector);
-     if (!DOM) {
-         return false;
-     }
-
-    let HTML = '';
     const countStartL = 0;
     const countEndL = Math.ceil(resumeData.data.length / 2);
     const countStartR = countEndL;
@@ -19,46 +12,38 @@ function renderResume(selector, resumeData) {
         if ( selector === '#resumeLeft' ) {
             for (let i = countStartL; i < countEndL; i++) {
                 const resume = resumeData.data[i];
-                if (!resume) {
-                    continue;
-                }
  
-            HTML += `<div class="resume-left-column">
-                        <div class="icon">
-                        <i class="fa fa-book" aria-hidden="true"></i>
-                        </div>
-                        <ul class="left-column">
-                        <li class="list">
-                            <span class="res-dot" class="res-dot">${resume.duration}</span>
-                            <h5>${resume.course}</h5>
-                            <h6>${resume.uni}</h6>
-                        </li>
-                    </ul>
-                    </div>`;
+            const leftHTML = `<ul class="left-column">
+                                <li class="list">
+                                    <span class="res-dot" class="res-dot">${resume.duration}</span>
+                                    <h5>${resume.course}</h5>
+                                    <h6>${resume.uni}</h6>
+                                </li>
+                            </ul>`;
 
+            const DOM = document.querySelector('.resume-left-column', '.icon');
+            DOM.insertAdjacentHTML('beforeend', leftHTML);
+               
         }        
     }
-    if ( selector === '#resumeRight' ) {
+     if ( selector === '#resumeRight' ) {
         for (let i = countStartR; i < countEndR; i++) {
             const resume = resumeData.data[i];
-            HTML += `<div class="resume-right-column">
-                        <div class="icon">
-                        <i class="fa fa-briefcase" aria-hidden="true"></i>
-                        </div>
-                        <ul class="right-column">
-                        <li class="list">
-                            <span class="res-dot">${resume.duration}</span>
-                            <h5>${resume.course}</h5>
-                            <h6>University Of Studies, Poland, Cracow</h6>
-                        </li>
-                        </ul>
-                    </div>`;
 
-        }
-    
-    }
+        const rightHTML = ` <ul class="right-column">
+                                <li class="list">
+                                    <span class="res-dot">${resume.duration}</span>
+                                    <h5>${resume.course}</h5>
+                                    <h6>University Of Studies, Poland, Cracow</h6>
+                                </li>
+                            </ul>`;
 
-    DOM.innerHTML = HTML;
+            const DOM = document.querySelector('.resume-right-column', '.icon');
+            DOM.insertAdjacentHTML('beforeend', rightHTML);
+         }
+     }
+
+    return true;
 }
 
 export { renderResume }
